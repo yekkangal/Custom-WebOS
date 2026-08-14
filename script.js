d41b48a5d2cc1f7312ea37cb5d1a78a8c91e2c1b
yekkangal <yek.kangal@gmail.com>
2026-08-14 16:05:57 +0000

    App WindowV1.1 (Try No.2)

--- script.js
+++ script.js
@@ -122,7 +122,7 @@ document.addEventListener("click", function() {
 
 dragElement(document.querySelector("#Notes-App"));
 
-var notesScreen = document.querySelector("#Notes-App")
+var notesScreen = document.querySelector("#Desktop-App1")
 
 var notesScreenClose = document.querySelector("#NotesClose")
 
